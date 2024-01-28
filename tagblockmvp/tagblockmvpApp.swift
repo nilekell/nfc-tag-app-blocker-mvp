f483b24f1f762d29cdbea0f141b512f8fa1f5f56
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-01-28 10:43:09 +0000

    Initial Commit

---
+++ tagblockmvp/tagblockmvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  tagblockmvpApp.swift
+//  tagblockmvp
+//
+//  Created by Nile Kelly on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct tagblockmvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
