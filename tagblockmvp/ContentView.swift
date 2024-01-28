c48de8dcf96c956e03b127e2ea16d5aa69d7dd4d
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-01-28 14:54:12 +0000

    wip nfc logic + show family activity picker

--- tagblockmvp/ContentView.swift
+++ tagblockmvp/ContentView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 struct ContentView: View {
+    
+    @State var isLockedMode = false
+    @State var selectionToDiscourage = FamilyActivitySelection()
+    @State var isPresented = false
+    @State var reader = NFCReader()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("App Blocker")
+                .padding()
+            Button("Select apps to block") { isPresented = true }
+                   .familyActivityPicker(isPresented: $isPresented,
+                                         selection: $selectionToDiscourage)
+                   .padding()
+            Button(action: {
+                reader.scan()
+            }, label: {
+                Text("Scan tag")
+            })
+            
+            // Displaying the scanned data
+            Text(reader.str)
+                .padding()
         }
         .padding()
     }
---
+++ tagblockmvp/NFCReader.swift
@@ -0,0 +1,101 @@
+//
+//  NFCReader.swift
+//  tagblockmvp
+//
+//  Created by Nile Kelly on 28/01/2024.
+//
+
+import Foundation
+import CoreNFC
+
+class NFCReader: NSObject, ObservableObject, NFCNDEFReaderSessionDelegate {
+    
+    var data: Data?
+    var nfcSession: NFCNDEFReaderSession?
+    var str: String = ""
+    
+    func scan() {
+        nfcSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.alertMessage = "Hold Your iPhone Near an NFC Card"
+        nfcSession?.begin()
+    }
+    
+    func invalidateSessionWithMessage(session: NFCNDEFReaderSession, message: String) {
+        session.alertMessage = message
+        session.invalidate()
+    }
+    
+    // called when the reader-session expired, you invalidated the dialog or accessed an invalidated session
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("Error reading NFC: \(error.localizedDescription)")
+    }
+    
+    // called when a reader session begins
+    func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
+        print("Reader session activated.")
+    }
+
+    // called when a new set of NDEF messages is found
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        DispatchQueue.main.async {
+            // Process first detected NFCNDEFMessage objects.
+            let firstNDEFMessage = messages[0]
+            self.data = firstNDEFMessage.records.first?.payload ?? Data()
+            self.str = String(decoding: self.data ?? Data(), as: UTF8.self)
+        }
+    }
+    
+    // called when a a tag is detected
+    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+        if tags.count > 1 {
+            let retryInterval = DispatchTimeInterval.milliseconds(500)
+            session.alertMessage = "More than one Tag Detected, please try again."
+            DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
+                session.restartPolling()
+            })
+            return
+        }
+        
+        let tag = tags.first!
+        session.connect(to: tag, completionHandler: {(error: Error?) in
+            if nil != error {
+                self.invalidateSessionWithMessage(session: session, message: "Unable to connect to tag.")
+                return
+            }
+            
+            tag.queryNDEFStatus(completionHandler: {(ndefStatus: NFCNDEFStatus, capacity: Int, error: Error?) in
+                guard error == nil else {
+                    self.invalidateSessionWithMessage(session: session, message: "Unable to connect to tag.")
+                    return
+                }
+                
+                switch ndefStatus {
+                case .notSupported:
+                    self.invalidateSessionWithMessage(session: session, message: "Unable to connect to tag. This tag is not supported.")
+                case .readOnly:
+                    self.invalidateSessionWithMessage(session: session, message: "Unable to connect to tag. This tag is read only.")
+                case .readWrite:
+                    tag.readNDEF(completionHandler: {(ndefMessage: NFCNDEFMessage?, error: Error?) in
+                        guard error == nil else {
+                            self.invalidateSessionWithMessage(session: session, message: "Unable to read data on tag.")
+                            return
+                        }
+                        
+                        guard ndefMessage == nil else {
+                            self.invalidateSessionWithMessage(session: session, message: "Unable to read data on tag.")
+                            return
+                        }
+                        
+                        guard !(ndefMessage?.records.isEmpty)!, ndefMessage!.records[0].typeNameFormat != .empty else {
+                            self.invalidateSessionWithMessage(session: session, message: "Unable to read data on tag.")
+                            return
+                        }
+                    })
+                    
+                @unknown default:
+                    self.invalidateSessionWithMessage(session: session, message: "Unable to read data on tag.")
+                }
+            })
+        })
+    }
+}
